76e3c3d28d5460d1994c2f9021e8bb8827bcddd3
Eduardo-PC <devugates@dronekids.com.br>
2020-03-25 15:36:35 -0700

    Early tutorial + Proximity GUI Change
    Early implementation of tutorials using text with delay to show up and disappear.
    Proximity GUI is no more. For now. In the future it may be used as a diegetic form of letting the player know how distant it is from the nearby planet. For now it calls are commented.

---
+++ Assets/PopUpBehaviour.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PopUpBehaviour : MonoBehaviour
+{
+    public float start_delay;
+    public float lifetime;
+    public AnimationCurve size_curve;
+
+    void Start ()
+    {
+        StartCoroutine(PopUpRoutine());
+    }
+
+    IEnumerator PopUpRoutine()
+    {
+        transform.localScale = new Vector3(0, 0, 0);
+
+        yield return new WaitForSecondsRealtime(start_delay);
+
+        float progress = 0;
+        while(progress <= 1)
+        {
+            float v = size_curve.Evaluate(progress);
+            transform.localScale = new Vector3(v, v, v);
+            progress += 0.02f;
+            yield return null;
+        }
+
+        yield return new WaitForSecondsRealtime(lifetime);
+
+        progress = 1;
+        while (progress >= 0)
+        {
+            float v = size_curve.Evaluate(progress);
+            transform.localScale = new Vector3(v, v, v);
+            progress -= 0.04f;
+            yield return null;
+        }
+
+        transform.localScale = new Vector3(0, 0, 0);
+
+        yield break;
+    }
+}
